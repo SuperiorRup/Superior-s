49bfc1f57cf278085a7bbdd7ac764457eff74443
SuperiorRup <46254794+SuperiorRup@users.noreply.github.com>
2019-02-07 10:33:36 +0530

    Update test(1).py

--- test(1).py
+++ test(1).py
@@ -141,6 +141,6 @@ async def unban(ctx, userName: discord.User):
         embed=discord.Embed(title="Command not accepted!", description="Sorry! You don't have permission to use this command.", color=0xff00f6)
         await client.say(embed=embed)
 
-client.run('NTQyOTE4ODY4NTI0MzM1MTE2.Dz1ArA.FCNR0hO3IMTl_pA9x99IjCGCIEE')
+client.run('NTQyNjQ5MjA0MTQxNDU3NDA5.Dz1NnA.Dz-JB831Nnhfb0Fqdi9K5o-b_1U')
 
 
